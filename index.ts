0dfcd9000c59feb90831df264b24b3a51ca1c743
Andrew Tomaka <andrew@oak.tech>
2022-09-09 09:43:02 -0400

    Pull out constants

--- index.ts
+++ index.ts
@@ -3,43 +3,54 @@ import { ApiPromise, WsProvider, Keyring } from '@polkadot/api';
 import { u8aToHex } from '@polkadot/util';
 import { XcmV1MultiLocation } from "@polkadot/types/lookup"
 
-async function main () {
-  const api = await ApiPromise.create({
-    provider: new WsProvider('wss://rpc.turing.oak.tech')
-  });
-  const keyring = new Keyring()
+const ALICE = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
+const OAK_PARA_ID = 2114;
+const SUBSTRATE_NETWORK = 42;
 
+const LOCAL_OAK_ENDPOINT = 'ws://localhost:9946';
+const LOCAL_TEM_ENDPOINT = 'ws://localhost:9947';
+
+async function main () {
+  const keyring = new Keyring();
+
+  // setup API
+  const oakApi = await ApiPromise.create({
+    provider: new WsProvider(LOCAL_OAK_ENDPOINT)
+  });
+  const temApi = await ApiPromise.create({
+    provider: new WsProvider(LOCAL_TEM_ENDPOINT)
+  });
+
+  // create correct multi-location
   const location = {
     parents: 1,
     interior: {
       X2: [
-        { Parachain: 2114 },
+        { Parachain: OAK_PARA_ID },
         {
           AccountId32: {
             network: "Any",
-            id: keyring.decodeAddress(
-              "5FeAonhz3VJb7RvhWvrLDfHu8m19jDZmhrZhfFR7dkPjmTwD"
-            ),
+            id: keyring.decodeAddress(ALICE),
           }
         }
       ]
     }
   };
-  const multilocation: XcmV1MultiLocation = api.createType(
+  const multilocation: XcmV1MultiLocation = oakApi.createType(
     "XcmV1MultiLocation",
     location
   );
 
+  // Find derived account
   const toHash = new Uint8Array([
     ...new Uint8Array([32]),
     ...new TextEncoder().encode("multiloc"),
     ...multilocation.toU8a(),
   ]);
+  const descendAddress = u8aToHex(oakApi.registry.hash(toHash).slice(0, 32));
 
-  const descendOriginAddress = u8aToHex(api.registry.hash(toHash).slice(0, 32));
-
-  // Substrate network id to match current RPC output
-  console.log(keyring.encodeAddress(descendOriginAddress, 42));
+  // Output wallet address (with substrate network to match RPC)
+  console.log(keyring.encodeAddress(descendAddress, SUBSTRATE_NETWORK));
 }
 
 
