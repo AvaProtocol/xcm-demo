9d1c3b1eb4a68ba1529443ce72be16d1127fa136
chrisli30 <chrisli30@users.noreply.github.com>
2022-11-22 13:04:40 -0800

    Revamp sendExtrinsic call in utils.js to use polkadot.js code

--- src/common/utils.js
+++ src/common/utils.js
@@ -4,26 +4,49 @@ export const sendExtrinsic = async (api, extrinsic, keyPair, { isSudo = false } 
 	return new Promise((resolve) => {
 
     const newExtrinsic = isSudo ? api.tx.sudo.sudo(extrinsic) : extrinsic;
-		newExtrinsic.signAndSend(keyPair, { nonce: -1 }, ({ status, dispatchError }) => {
+		newExtrinsic.signAndSend(keyPair, { nonce: -1 }, ({ status, events, dispatchError }) => {
 			console.log("status.type", status.type);
-			if (status.type === 'Finalized') {
-				console.log('Finalize extrinsic in block: ', status.asFinalized.toString());
 
-				if (!_.isNil(dispatchError)) {
-					if (dispatchError.isModule) {
-						const metaError = api.registry.findMetaError(dispatchError.asModule)
-						const { docs, name, section } = metaError
-						const dispatchErrorMessage = JSON.stringify({ docs, name, section })
-						const errMsg = `Transaction finalized with error by blockchain ${dispatchErrorMessage}`
-						console.log(errMsg)
+			if (status.isInBlock || status.isFinalized) {
+				events
+				// find/filter for failed events
+				.filter(({ event }) =>api.events.system.ExtrinsicFailed.is(event))
+				// we know that data for system.ExtrinsicFailed is
+				// (DispatchError, DispatchInfo)
+				.forEach(({ event: { data: [error, info] } }) => {
+					if (error.isModule) {
+						// for module errors, we have the section indexed, lookup
+						const decoded = api.registry.findMetaError(error.asModule);
+						const { docs, method, section } = decoded;
+						console.log(`${section}.${method}: ${docs.join(' ')}`);
 					} else {
-						const errMsg = `Transaction finalized with error by blockchain ${dispatchError.toString()}`
-						console.log(errMsg)
+						// Other, CannotLookup, BadOrigin, no extra info
+						console.log(error.toString());
 					}
-				}
+				});
 
-				resolve(status.asFinalized.toString());
+				if(status.isFinalized){
+					return resolve(status.asFinalized.toString());
+				}
 			}
+			// if (status.type === 'Finalized') {
+			// 	console.log('Finalize extrinsic in block: ', status.asFinalized.toString());
+
+			// 	if (!_.isNil(dispatchError)) {
+			// 		if (dispatchError.isModule) {
+			// 			const metaError = api.registry.findMetaError(dispatchError.asModule)
+			// 			const { docs, name, section } = metaError
+			// 			const dispatchErrorMessage = JSON.stringify({ docs, name, section })
+			// 			const errMsg = `Transaction finalized with error by blockchain ${dispatchErrorMessage}`
+			// 			console.log(errMsg)
+			// 		} else {
+			// 			const errMsg = `Transaction finalized with error by blockchain ${dispatchError.toString()}`
+			// 			console.log(errMsg)
+			// 		}
+			// 	}
+
+			// 	resolve(status.asFinalized.toString());
+			// }
 		});
 	});
 }
