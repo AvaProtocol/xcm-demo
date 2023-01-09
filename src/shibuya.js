7e73b161079be8265140037be6258fae9f4c1ac8
imstar15 <imstar15@qq.com>
2023-01-09 10:01:23 +0800

    Transfer amount to proxy account

--- src/shibuya.js
+++ src/shibuya.js
@@ -43,30 +43,39 @@ const getProxyAccount = (api, address) => {
 }
 
 const main = async () => {
-	await turingHelper.initialize(TURING_ENDPOINT);
-	await shibuyaHelper.initialize(SHIBUYA_ENDPOINT);
+  await turingHelper.initialize(TURING_ENDPOINT);
+  await shibuyaHelper.initialize(SHIBUYA_ENDPOINT);
 
-	const keyring = new Keyring();
-	const aliceKeyring = keyring.addFromUri(`//Alice`, undefined, 'sr25519');
-	const alicePublicKey = aliceKeyring.address;
+  const keyring = new Keyring();
+  const aliceKeyring = keyring.addFromUri(`//Alice`, undefined, 'sr25519');
+  const alicePublicKey = aliceKeyring.address;
 
-	const shibuyaAddress = keyring.encodeAddress(alicePublicKey, chainConfig.shibuya.ss58);
+  const shibuyaAddress = keyring.encodeAddress(alicePublicKey, chainConfig.shibuya.ss58);
   // const turingAddress = keyring.encodeAddress(alicePublicKey, chainConfig.turing.ss58);
   const proxyAccount = getProxyAccount(shibuyaHelper.api, shibuyaAddress);
-  
-  // Add proxy
+
+  // // Add proxy
+  console.log(`\n1. Add proxy`);
   await sendExtrinsic(shibuyaHelper.api, shibuyaHelper.api.tx.proxy.addProxy(proxyAccount, "Any", 0), aliceKeyring);
 
-	const proxyExtrinsic = shibuyaHelper.api.tx.system.remarkWithEvent('Hello!!!');
-	const shibuyaProxyCall = shibuyaHelper.api.tx.proxy.proxy(shibuyaAddress, 'Any', proxyExtrinsic);
-	
+  // Transfer amount to proxy account
+  console.log(`\n2. Transfer amount to proxy account`);
+  const transferExtrinsic = shibuyaHelper.api.tx.balances.transfer(proxyAccount, '1000000000000000000000');
+  await sendExtrinsic(shibuyaHelper.api, transferExtrinsic, aliceKeyring);
+
+  // Create the call for scheduleXcmpTask
+  console.log(`\n3. Create the call for scheduleXcmpTask `);
+  const proxyExtrinsic = shibuyaHelper.api.tx.system.remarkWithEvent('Hello!!!');
+  const shibuyaProxyCall = shibuyaHelper.api.tx.proxy.proxy(shibuyaAddress, 'Any', proxyExtrinsic);
+  
   const encodedShibuyaProxyCall = shibuyaProxyCall.method.toHex(shibuyaProxyCall);
   const shibuyaProxyCallFees = await shibuyaProxyCall.paymentInfo(shibuyaAddress);
 
   console.log('encodedShibuyaProxyCall: ', encodedShibuyaProxyCall);
   console.log('shibuyaProxyCallFees: ', shibuyaProxyCallFees.toHuman());
 
-  console.log(`\n1. Create the call for scheduleXcmpTask `);
+  // Schedule xcmp task
+  console.log('\n4. Create scheduleXcmpTask extrinsic');
   const providedId = "xcmp_automation_test_" + (Math.random() + 1).toString(36).substring(7);
   const xcmpCall = turingHelper.api.tx.automationTime.scheduleXcmpTask(
     providedId,
@@ -74,12 +83,13 @@ const main = async () => {
     SHIBUYA_PARA_ID,
     0,
     encodedShibuyaProxyCall,
+    // 6191761979,
     parseInt(shibuyaProxyCallFees.weight.refTime),
   );
 
   console.log('xcmpCall: ', xcmpCall);
 
-  console.log(`\n3. Sign and send scheduleXcmpTask call ...`);
+  console.log(`\n5. Sign and send scheduleXcmpTask call ...`);
   await sendExtrinsic(turingHelper.api, xcmpCall, aliceKeyring);
 };
 
