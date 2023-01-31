15a3aba0f21fc202949d681615f9b52f5b4d7949
chrisli30 <chrisli30@users.noreply.github.com>
2023-01-31 01:11:57 -0800

    Proposed config change to replace env with more structured chain parameters

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+import RococoDev from './rococo-dev';
+import MangataDev from './mangata-dev';
+import TuringDev from './turing-dev';
+import Shibuya from './shibuya';
+
+export {
+    RococoDev, MangataDev, TuringDev, Shibuya,
+};
