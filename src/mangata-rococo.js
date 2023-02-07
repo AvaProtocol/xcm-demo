9ad192eeba733546a9507077e986771fc1f5d576
imstar15 <imstar15@qq.com>
2023-02-07 21:38:17 +0800

    Cancel mangata task

--- src/mangata-rococo.js
+++ src/mangata-rococo.js
@@ -9,7 +9,7 @@ import TuringHelper from './common/turingHelper';
 import MangataHelper from './common/mangataHelper';
 import Account from './common/account';
 import {
-    delay, listenEvents, readMnemonicFromFile, getDecimalBN,
+    delay, listenEvents, readMnemonicFromFile, getDecimalBN, sendExtrinsic, calculateTimeout,
 } from './common/utils';
 import {
     TuringStaging, MangataRococo,
@@ -192,8 +192,16 @@ async function main() {
             await turingHelper.sendXcmExtrinsic(xcmpCall, account.pair, taskId);
 
             // Listen XCM events on Mangata side
-            console.log('\nd) waiting for XCM events on Mangata side ...');
-            await listenEvents(mangataHelper.api);
+            console.log(`\n4. Keep Listening XCM events on ${mangataChainName} until ${moment(timestampNextHour * 1000).format('YYYY-MM-DD HH:mm:ss')}(${timestampNextHour}) to verify that the task(taskId: ${taskId}, providerId: ${providedId}) will be successfully executed ...`);
+
+            const nextHourTimeout = calculateTimeout(timestampNextHour);
+            const isTaskExecuted = await listenEvents(mangataHelper.api, 'proxy', 'ProxyExecuted', nextHourTimeout);
+            if (!isTaskExecuted) {
+                console.log('Timeout! Task was not executed.');
+                return;
+            }
+
+            console.log('Task has been executed!');
 
             console.log('\nWaiting 20 seconds before reading new chain states ...');
             await delay(20000);
@@ -203,6 +211,20 @@ async function main() {
             console.log(`\nAfter auto-compound, reserved ${poolName} is: ${newLiquidityBalance.reserved.toString()} planck ...`);
 
             console.log(`${account.name} has compounded ${(newLiquidityBalance.reserved.sub(liquidityBalance.reserved)).toString()} planck more ${poolName} ...`);
+
+            console.log('\n5. Cancel task ...');
+            const cancelTaskExtrinsic = turingHelper.api.tx.automationTime.cancelTask(taskId);
+            await sendExtrinsic(turingHelper.api, cancelTaskExtrinsic, keyPair);
+
+            console.log(`\n6. Keep Listening events on ${mangataChainName} until ${moment(timestampTwoHoursLater * 1000).format('YYYY-MM-DD HH:mm:ss')}(${timestampTwoHoursLater}) to verify that the task was successfully canceled ...`);
+
+            const twoHoursLaterExecutionTimeout = calculateTimeout(timestampTwoHoursLater);
+            const isTaskExecutedAgain = await listenEvents(mangataHelper.api, 'proxy', 'ProxyExecuted', twoHoursLaterExecutionTimeout);
+            if (isTaskExecutedAgain) {
+                console.log('Task cancellation failed! It executes again.');
+                return;
+            }
+            console.log("Task canceled successfully! It didn't execute again.");
         }
     }
 }
