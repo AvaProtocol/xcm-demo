9c0fa9ea6c8f204af18bb199ea12d56e3b55f4f6
imstar15 <imstar15@qq.com>
2023-02-11 01:35:05 +0800

    Add comment for MIN_BALANCE_IN_PROXY

--- src/astar/constants.js
+++ src/astar/constants.js
@@ -1,5 +1,6 @@
 // One XCM operation is 1_000_000_000 weight - almost certainly a conservative estimate.
 // It is defined as a UnitWeightCost variable in runtime.
 export const TURING_INSTRUCTION_WEIGHT = 1000000000;
-export const TASK_FREQUENCY = 3600;
+// The proxy accounts are to be topped up if its balance fails below this number
 export const MIN_BALANCE_IN_PROXY = 10;
+export const TASK_FREQUENCY = 3600;
