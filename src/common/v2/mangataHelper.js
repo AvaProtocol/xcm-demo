929eb7d17bc0e5570bcb120b5e6a1b5d9c670f8f
imstar15 <imstar15@qq.com>
2023-09-04 14:30:54 +0800

    Fix setup-mangata

--- src/common/v2/mangataHelper.js
+++ src/common/v2/mangataHelper.js
@@ -2,8 +2,9 @@ import _ from 'lodash';
 import BN from 'bn.js';
 import { Mangata } from '@mangata-finance/sdk';
 import { getDecimalBN, sendExtrinsic } from '../utils';
+import ChainHelper from './chainHelper';
 
-class MangataHelper {
+class MangataHelper extends ChainHelper {
     initialize = async () => {
         this.mangataSdk = Mangata.getInstance([this.config.endpoint]);
         this.api = await this.mangataSdk.getApi();
@@ -12,6 +13,11 @@ class MangataHelper {
     getMangataSdk() {
         return this.mangataSdk;
     }
+
+    initIssuance = async (keyPair) => {
+        await sendExtrinsic(this.api, this.api.tx.issuance.finalizeTge(), keyPair, { isSudo: true });
+        await sendExtrinsic(this.api, this.api.tx.issuance.initIssuanceConfig(), keyPair, { isSudo: true });
+    };
 
     calculateRewardsAmount = async (address, liquidityAsset) => {
         const amountBN = await this.mangataSdk.calculateRewardsAmount(address, _.toString(liquidityAsset.id));
@@ -38,11 +44,6 @@ class MangataHelper {
         return _.extend(pool, { firstTokenAmountFloat, secondTokenAmountFloat });
     };
 
-    initIssuance = async (keyPair) => {
-        await sendExtrinsic(this.api, this.api.tx.issuance.finalizeTge(), keyPair, { isSudo: true });
-        await sendExtrinsic(this.api, this.api.tx.issuance.initIssuanceConfig(), keyPair, { isSudo: true });
-    };
-
     mintToken = async (accountId, assetId, keyPair, amount = 5000000000000000) => {
         const mintTokenExtrinsic = this.api.tx.tokens.mint(assetId, accountId, amount);
         await sendExtrinsic(this.api, mintTokenExtrinsic, keyPair, { isSudo: true });
@@ -54,86 +55,56 @@ class MangataHelper {
         return filterdPools;
     };
 
-    getTokenIdBySymbol(symbol) {
-        throw new Error('Method not implement');
-        // const token = _.find(this.assets, { symbol });
-        // return token.id;
-    }
-
     createPool = async ({
-        firstTokenId, firstAmount, secondTokenId, secondAmount, keyringPair,
-    }) => {
-        throw new Error('Method not implement');
-        // const firstToken = _.find(this.assets, { id: firstTokenId });
-        // const firstTokenDecimals = getDecimalBN(firstToken.decimals);
-        // const firstAmountBN = (new BN(firstAmount)).mul(firstTokenDecimals);
+        firstAssetId, firstAmount, secondAssetId, secondAmount, keyringPair,
+    }) => this.mangataSdk.createPool(keyringPair, _.toString(firstAssetId), firstAmount, _.toString(secondAssetId), secondAmount);
 
-        // const secondToken = _.find(this.assets, { id: secondTokenId });
-        // const secondTokenDecimals = getDecimalBN(secondToken.decimals);
-        // const secondAmountBN = (new BN(secondAmount)).mul(secondTokenDecimals);
-
-        // return this.mangata.createPool(keyPair, _.toString(firstTokenId), firstAmountBN, _.toString(secondTokenId), secondAmountBN);
-    };
-
-    updatePoolPromotion = async(tokenId, liquidityMiningIssuanceWeight, keyPair) => {
-        throw new Error('Method not implement');
-        // const promotePoolExtrinsic = this.api.tx.proofOfStake.updatePoolPromotion(tokenId, liquidityMiningIssuanceWeight);
-        // await sendExtrinsic(this.api, promotePoolExtrinsic, keyPair, { isSudo: true });
+    updatePoolPromotion = async (tokenId, liquidityMiningIssuanceWeight, keyringPair) => {
+        const promotePoolExtrinsic = this.api.tx.proofOfStake.updatePoolPromotion(tokenId, liquidityMiningIssuanceWeight);
+        await sendExtrinsic(this.api, promotePoolExtrinsic, keyringPair, { isSudo: true });
     };
 
     mintLiquidity = async ({
         pair, firstTokenId, firstTokenAmount, secondTokenId, expectedSecondTokenAmount,
-    }) => {
-        throw new Error('Method not implement');
-        // const firstToken = _.find(this.assets, { id: firstTokenId });
-        // const firstDecimalBN = getDecimalBN(firstToken.decimals);
+    }) => this.mangataSdk.mintLiquidity(pair, firstTokenId, secondTokenId, firstTokenAmount, expectedSecondTokenAmount)
+        .then((events) => {
+            const lastEvent = _.last(events);
 
-        // const secondToken = _.find(this.assets, { id: secondTokenId });
-        // const secondDecimalBN = getDecimalBN(secondToken.decimals);
+            if (lastEvent.section === 'system' && lastEvent.method === 'ExtrinsicFailed') {
+                const txPaymentEvent = _.find(events, (event) => {
+                    const { section, method } = event;
+                    return section === 'transactionPayment' && method === 'TransactionFeePaid';
+                });
 
-        // const amountBN = (new BN(firstTokenAmount, 10)).mul(firstDecimalBN);
-        // const expectedSecondAmountBN = (new BN(expectedSecondTokenAmount)).mul(secondDecimalBN);
+                const error = new Error(lastEvent?.error?.documentation);
+                error.feePayer = txPaymentEvent?.eventData[0]?.data;
+                throw error;
+            } if (lastEvent.section === 'system' && lastEvent.method === 'ExtrinsicSuccess') {
+                const matchedEvent = _.find(events, (event) => {
+                    if (event.section === 'xyk' && event.method === 'LiquidityMinted') {
+                        return event;
+                    }
 
-        // return this.mangata.mintLiquidity(pair, firstTokenId, secondTokenId, amountBN, expectedSecondAmountBN)
-        //     .then((events) => {
-        //         const lastEvent = _.last(events);
+                    return undefined;
+                });
 
-        //         if (lastEvent.section === 'system' && lastEvent.method === 'ExtrinsicFailed') {
-        //             const txPaymentEvent = _.find(events, (event) => {
-        //                 const { section, method } = event;
-        //                 return section === 'transactionPayment' && method === 'TransactionFeePaid';
-        //             });
+                if (!_.isUndefined(matchedEvent)) {
+                    const address = matchedEvent.eventData[0]?.data;
+                    const firstId = new BN(matchedEvent.eventData[1]?.data);
+                    const secondId = new BN(matchedEvent.eventData[3]?.data);
+                    const firstAmount = new BN(matchedEvent.eventData[2]?.data);
+                    const secondAmount = new BN(matchedEvent.eventData[4]?.data);
 
-        //             throw {
-        //                 message: lastEvent?.error?.documentation, feePayer: txPaymentEvent?.eventData[0]?.data,
-        //             };
-        //         } if (lastEvent.section === 'system' && lastEvent.method === 'ExtrinsicSuccess') {
-        //             const matchedEvent = _.find(events, (event) => {
-        //                 if (event.section === 'xyk' && event.method === 'LiquidityMinted') {
-        //                     return event;
-        //                 }
+                    return {
+                        module: 'xyk.LiquidityMinted', address, firstId: firstId.toNumber(), firstAmount: firstAmount.toString(), secondId: secondId.toNumber(), secondAmount: secondAmount.toString(),
+                    };
+                }
 
-        //                 return undefined;
-        //             });
+                return events;
+            }
 
-        //             if (!_.isUndefined(matchedEvent)) {
-        //                 const address = matchedEvent.eventData[0]?.data;
-        //                 const firstId = new BN(matchedEvent.eventData[1]?.data);
-        //                 const secondId = new BN(matchedEvent.eventData[3]?.data);
-        //                 const firstAmount = new BN(matchedEvent.eventData[2]?.data);
-        //                 const secondAmount = new BN(matchedEvent.eventData[4]?.data);
-
-        //                 return {
-        //                     module: 'xyk.LiquidityMinted', address, firstId: firstId.toNumber(), firstAmount: firstAmount.toString(), secondId: secondId.toNumber(), secondAmount: secondAmount.toString(),
-        //                 };
-        //             }
-
-        //             return events;
-        //         }
-
-        //         return events;
-        //     });
-    };
+            return events;
+        });
 }
 
 export default MangataHelper;
--- src/mangata/setup-mangata.js
+++ src/mangata/setup-mangata.js
@@ -6,9 +6,10 @@ import { Keyring } from '@polkadot/api';
 import { chains } from '@oak-network/config';
 import { u8aToHex } from '@polkadot/util';
 import { MangataAdapter, OakAdapter } from '@oak-network/adapter';
+import { BN } from 'bn.js';
 import OakHelper from '../common/v2/oakHelper';
 import MangataHelper from '../common/v2/mangataHelper';
-import { delay } from '../common/utils';
+import { delay, getDecimalBN } from '../common/utils';
 
 /**
  * README!
@@ -35,7 +36,7 @@ async function main() {
     await mangataHelper.initialize();
     const mangataApi = mangataHelper.getApi();
     const mangataSdk = mangataHelper.getMangataSdk();
-    const mangataAdapter = new MangataAdapter(mangataApi, turingLocal);
+    const mangataAdapter = new MangataAdapter(mangataApi, mangataLocal);
 
     const oakChainName = oakAdapter.getChainData().key;
     const mangataChainName = mangataAdapter.getChainData().key;
@@ -50,18 +51,12 @@ async function main() {
     const keyringPair = keyring.addFromUri('//Alice', undefined, 'sr25519');
     keyringPair.meta.name = 'Alice';
 
-    const mangataAddress = keyring.encodeAddress(keyringPair, mangataAdapter.getChainData().ss58Prefix);
+    const mangataAddress = keyring.encodeAddress(keyringPair.addressRaw, mangataAdapter.getChainData().ss58Prefix);
 
     let assets = await mangataSdk.getAssetsInfo();
     assets = _.map(assets, (asset) => asset);
     const oakAsset = _.find(assets, { symbol: oakDefaultAsset.symbol });
     const mangataAsset = _.find(assets, { symbol: mangataDefaultAsset.symbol });
-
-    const pools = await mangataSdk.getPools();
-    let targetPool = _.find(pools, { firstTokenId: mangataAsset.id, secondTokenId: oakAsset.id });
-    targetPool = mangataHelper.formatPool(targetPool);
-    const liquidityAsset = _.find(assets, { id: targetPool.liquidityTokenId });
-    const { name: poolName } = liquidityAsset;
 
     console.log('\n2. Initing inssuance on Mangata ...');
     await mangataHelper.initIssuance(keyringPair);
@@ -69,102 +64,101 @@ async function main() {
     console.log(`\n3. Minting ${oakAsset.name} for ${keyringPair.meta.name} on Maganta if balance is zero ...`);
     await mangataHelper.mintToken(u8aToHex(keyringPair.addressRaw), oakAsset.id, keyringPair);
 
-    // If there is no proxy, add a proxy of Turing on Mangata
-    console.log('\n4. Add a proxy on Mangata for paraId 2114, or skip this step if that exists ...');
+    // // If there is no proxy, add a proxy of Turing on Mangata
+    // console.log('\n4. Add a proxy on Mangata for paraId 2114, or skip this step if that exists ...');
 
-    const proxyAccountId = mangataAdapter.getDerivativeAccount(u8aToHex(keyringPair.addressRaw), oakAdapter.getChainData().paraId);
-    const proxyAddress = keyring.encodeAddress(proxyAccountId, mangataAdapter.getChainData().ss58Prefix);
-    const proxiesResponse = await mangataHelper.api.query.proxy.proxies(mangataAddress);
-    const proxies = _.first(proxiesResponse.toJSON());
+    // const proxyAccountId = mangataAdapter.getDerivativeAccount(u8aToHex(keyringPair.addressRaw), oakAdapter.getChainData().paraId);
+    // const proxyAddress = keyring.encodeAddress(proxyAccountId, mangataAdapter.getChainData().ss58Prefix);
+    // const proxiesResponse = await mangataHelper.api.query.proxy.proxies(mangataAddress);
+    // const proxies = _.first(proxiesResponse.toJSON());
 
-    const proxyType = 'AutoCompound';
-    const matchCondition = { delegate: proxyAddress, proxyType };
+    // const proxyType = 'AutoCompound';
+    // const matchCondition = { delegate: proxyAddress, proxyType };
 
-    const proxyMatch = _.find(proxies, matchCondition);
+    // const proxyMatch = _.find(proxies, matchCondition);
 
-    if (proxyMatch) {
-        console.log(`Found proxy of ${mangataAddress} on Mangata: `, proxyMatch);
-    } else {
-        if (_.isEmpty(proxies)) {
-            console.log(`Proxy array of ${mangataAddress} is empty ...`);
-        } else {
-            console.log('Proxy not found. Expected', matchCondition, 'Actual', proxies);
-        }
+    // if (proxyMatch) {
+    //     console.log(`Found proxy of ${mangataAddress} on Mangata: `, proxyMatch);
+    // } else {
+    //     if (_.isEmpty(proxies)) {
+    //         console.log(`Proxy array of ${mangataAddress} is empty ...`);
+    //     } else {
+    //         console.log('Proxy not found. Expected', matchCondition, 'Actual', proxies);
+    //     }
 
-        console.log(`Adding a proxy for paraId ${oakAdapter.getChainData().paraId}. Proxy address: ${proxyAddress} ...`);
-        await mangataHelper.addProxy(proxyAddress, proxyType, keyringPair);
-    }
+    //     console.log(`Adding a proxy for paraId ${oakAdapter.getChainData().paraId}. Proxy address: ${proxyAddress} ...`);
+    //     await mangataHelper.addProxy(proxyAddress, proxyType, keyringPair);
+    // }
 
     const answerPool = await confirm({ message: '\nAccount setup is completed. Press ENTRE to set up pools.', default: true });
+    if (!answerPool) return;
 
-    if (answerPool) {
-        // Get current pools available
-        const availablePools = await mangataHelper.getPools({ isPromoted: false });
-        console.log('\n5. AvailablePools pools: ', pools);
+    // Get current pools available
+    const availablePools = await mangataHelper.getPools({ isPromoted: false });
+    console.log('\n5. AvailablePools pools: ', availablePools);
 
-        const poolFound = _.find(availablePools, (pool) => pool.firstTokenId === mangataAsset.id && pool.secondTokenId === oakAsset.id);
+    const poolFound = _.find(availablePools, (pool) => pool.firstTokenId === mangataAsset.id && pool.secondTokenId === oakAsset.id);
 
-        // Create a MGR-TUR pool is not found
-        if (_.isUndefined(poolFound)) {
-            console.log(`No ${poolName} pool found; creating a ${poolName} pool with ${keyringPair.meta.name} ...`);
+    // Create a MGR-TUR pool is not found
+    if (_.isUndefined(poolFound)) {
+        console.log(`The pools for ${mangataAsset.symbol} and ${oakAsset.symbol} have not been found. Creating the pool with ${keyringPair.meta.name} ...`);
 
-            const parachainTokenDeposit = 10000; // Add 10,000 initial MGR to pool
-            const turingTokenDeposit = 1000; // Add 1,000 initial TUR to pool
+        const parachainTokenDeposit = 10000; // Add 10,000 initial MGR to pool
+        const turingTokenDeposit = 1000; // Add 1,000 initial TUR to pool
 
-            await mangataHelper.createPool({
-                firstTokenId: mangataAsset.id,
-                firstAmount: parachainTokenDeposit,
-                secondTokenId: oakAsset.id,
-                secondAmount: turingTokenDeposit,
-                keyringPair,
-            });
-
-            // Update assets
-            console.log(`\nChecking out assets after pool creation; there should be a new ${poolName} token ...`);
-            await mangataHelper.updateAssets();
-        } else {
-            console.log(`An existing ${poolName} pool found; skip pool creation ...`);
-        }
-
-        const poolsRetry = await mangataHelper.getPools({ isPromoted: false });
-
-        const pool = _.find(poolsRetry, { firstTokenId: mangataAsset.id, secondTokenId: oakAsset.id });
-        console.log(`Found a pool of ${poolName}`, pool);
-
-        // Promote pool
-        console.log('\n6. Promote the pool to activate liquidity rewarding ...');
-        await mangataHelper.updatePoolPromotion(pool.liquidityTokenId, 100, keyringPair);
-
-        // Mint liquidity to generate rewards...
-        console.log('\n7. Mint liquidity to generate rewards...');
-
-        const firstTokenAmount = 1000;
-        const MAX_SLIPPIAGE = 0.04; // 4% slippage; can’t be too large
-        const poolRatio = pool.firstTokenAmountFloat / pool.secondTokenAmountFloat;
-        const expectedSecondTokenAmount = (firstTokenAmount / poolRatio) * (1 + MAX_SLIPPIAGE);
-
-        // Estimate of fees; no need to be accurate
-        const fees = await mangataHelper.getMintLiquidityFee({
-            pair: keyringPair, firstTokenId: mangataAsset.id, firstTokenAmount, secondTokenId: oakAsset.id, expectedSecondTokenAmount,
+        await mangataHelper.createPool({
+            firstAssetId: mangataAsset.id,
+            firstAmount: new BN(parachainTokenDeposit).mul(getDecimalBN(mangataAsset.decimals)),
+            secondAssetId: oakAsset.id,
+            secondAmount: new BN(turingTokenDeposit).mul(getDecimalBN(oakAsset.decimals)),
+            keyringPair,
         });
-
-        console.log(`Mint Liquidity Fee: ${fees} ${mangataAsset.symbol}`);
-
-        await mangataHelper.mintLiquidity({
-            pair: keyringPair,
-            firstTokenId: mangataAsset.id,
-            firstTokenAmount: firstTokenAmount - fees,
-            secondTokenId: oakAsset.id,
-            expectedSecondTokenAmount,
-        });
-
-        console.log('\nWaiting 120 seconds to check claimable reward ...');
-        await delay(120 * 1000);
-
-        const liquidityTokenId = mangataHelper.getTokenIdBySymbol(poolName);
-        const rewardAmount = await mangataHelper.calculateRewardsAmount(mangataAddress, liquidityTokenId);
-        console.log(`Claimable reward in ${poolName}: `, rewardAmount);
+    } else {
+        const liquidityAsset = _.find(assets, { id: poolFound.liquidityTokenId });
+        console.log(`An existing ${liquidityAsset.symbol} pool found; skip pool creation ...`);
     }
+
+    // Update assets
+    assets = await mangataSdk.getAssetsInfo();
+
+    // // Find pool
+    const poolsRetry = await mangataHelper.getPools({ isPromoted: false });
+    const pool = _.find(poolsRetry, { firstTokenId: mangataAsset.id, secondTokenId: oakAsset.id });
+    const liquidityAsset = _.find(assets, { id: pool.liquidityTokenId });
+    console.log(`Found a pool of ${liquidityAsset.symbol}`, pool);
+
+    // Promote pool
+    console.log('\n6. Promote the pool to activate liquidity rewarding ...');
+    await mangataHelper.updatePoolPromotion(pool.liquidityTokenId, 100, keyringPair);
+
+    // Mint liquidity to generate rewards...
+    console.log('\n7. Mint liquidity to generate rewards...');
+    const formatedPool = mangataHelper.formatPool(pool, mangataAsset.decimals, oakAsset.decimals);
+    const firstTokenAmount = 1000;
+    const MAX_SLIPPIAGE = 0.04; // 4% slippage; can’t be too large
+    const poolRatio = formatedPool.firstTokenAmountFloat / formatedPool.secondTokenAmountFloat;
+    const expectedSecondTokenAmount = (firstTokenAmount / poolRatio) * (1 + MAX_SLIPPIAGE);
+
+    // Estimate of fees; no need to be accurate
+    const fees = await mangataHelper.getMintLiquidityFee({
+        pair: keyringPair, firstAsset: mangataAsset, firstTokenAmount, secondAsset: oakAsset, expectedSecondTokenAmount,
+    });
+
+    console.log(`Mint Liquidity Fee: ${fees} ${mangataAsset.symbol}`);
+
+    await mangataHelper.mintLiquidity({
+        pair: keyringPair,
+        firstTokenId: mangataAsset.id,
+        firstTokenAmount: new BN(firstTokenAmount - fees).mul(getDecimalBN(mangataAsset.decimals)),
+        secondTokenId: oakAsset.id,
+        expectedSecondTokenAmount: new BN(expectedSecondTokenAmount).mul(getDecimalBN(oakAsset.decimals)),
+    });
+
+    console.log('\nWaiting 120 seconds to check claimable reward ...');
+    await delay(120 * 1000);
+
+    const rewardAmount = await mangataHelper.calculateRewardsAmount(mangataAddress, liquidityAsset);
+    console.log(`Claimable reward in ${liquidityAsset.symbol}: `, rewardAmount);
 }
 
 main().catch(console.error).finally(() => {
